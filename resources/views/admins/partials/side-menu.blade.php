f23370d00b40a1f71a3ad9402f4cef271eb8ef3c
unknown <mohamed_tharwat@germaniatek.net>
2024-05-23 20:31:54 +0300

    updates

--- resources/views/admins/partials/side-menu.blade.php
+++ resources/views/admins/partials/side-menu.blade.php
@@ -2,13 +2,13 @@
     <div data-simplebar class="h-100">
         <div id="sidebar-menu">
             <ul class="metismenu list-unstyled" id="side-menu" style="margin-top:45px">
-                <li>
+                <li class="d-none">
                     <a href="{{route('dashboard.roles.index')}}">
                         <i class="bx bx-layout"></i>
                         <span key="t-maps">  الأدوار </span>
                     </a>
                 </li>
-                <li>
+                <li class="d-none">
                     <a href="{{route('dashboard.Permissions.index')}}">
                         <i class="bx bx-layout"></i>
                         <span key="t-maps">  الصلاحيات </span>
